ee0cbc0715fbfb108d472976cad9a98b1f908adb
Prince <princejnu7@hotmail.com>
2023-01-20 14:01:33 +0530

    new update

--- src/SnakeFrame.java
+++ src/SnakeFrame.java
@@ -1,12 +1,5 @@
 
 import javax.swing.*;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Prince kumar
@@ -15,25 +8,13 @@ import javax.swing.*;
 public class SnakeFrame extends JFrame {
     
 	SnakeFrame(){
-
-			
-
 		this.add(new SnakePanel());
-
 		this.setTitle("Snake");
-
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 		this.setResizable(false);
-
 		this.pack();
-
 		this.setVisible(true);
-
 		this.setLocationRelativeTo(null);
-
-		
-
 	}
 
 //    private void add(SnakePanel snakePanel) {
--- src/SnakePanel.java
+++ src/SnakePanel.java
@@ -7,56 +7,29 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
 
-
-
 public class SnakePanel extends JPanel implements ActionListener{
 
-
-
 	static final int SCREEN_WIDTH = 1300;
-
 	static final int SCREEN_HEIGHT = 750;
-
 	static final int UNIT_SIZE = 50;
-
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);
-
 	static final int DELAY = 175;
-
 	final int x[] = new int[GAME_UNITS];
-
 	final int y[] = new int[GAME_UNITS];
-
 	int bodyParts = 6;
-
-	int applesEaten;
-
-	int appleX;
-
-	int appleY;
-
+	int applesEaten, appleX, appleY;
 	char direction = 'R';
-
 	boolean running = false;
-
 	Timer timer;
-
 	Random random;
-
-	
 
 	SnakePanel(){
 
 		random = new Random();
-
 		this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
-
 		this.setBackground(Color.black);
-
 		this.setFocusable(true);
-
 		this.addKeyListener(new MyKeyAdapter());
-
 		startGame();
 
 	}
