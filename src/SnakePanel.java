985ed27da9d076fcdfc0558eb58e6f9dfb942447
Prince <princejnu7@hotmail.com>
2023-01-22 23:15:40 +0530

    new

--- src/SnakePanel.java
+++ src/SnakePanel.java
@@ -35,49 +35,29 @@ public class SnakePanel extends JPanel implements ActionListener{
 	}
 
 	public void startGame() {
-
 		newApple();
-
 		running = true;
-
 		timer = new Timer(DELAY,this);
-
 		timer.start();
-
 	}
 
 	public void paintComponent(Graphics g) {
-
 		super.paintComponent(g);
-
 		draw(g);
-
 	}
-
 	public void draw(Graphics g) {
-
-		
-
 		if(running) {
-
 			/*
 
 			for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
-
 				g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-
 				g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
 
 			}
-
 			*/
 
 			g.setColor(Color.red);
-
 			g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
-
-		
-
 			for(int i = 0; i< bodyParts;i++) {
 
 				if(i == 0) {
