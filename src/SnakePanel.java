129f96207c5892894da57d1526022bccd01976ed
Prince <princejnu7@hotmail.com>
2023-01-25 09:00:35 +0530

    new

--- src/SnakePanel.java
+++ src/SnakePanel.java
@@ -80,91 +80,45 @@ public class SnakePanel extends JPanel implements ActionListener{
 	}
 
 	public void newApple(){
-
 		appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
-
 		appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
-
 	}
-
 	public void move(){
-
 		for(int i = bodyParts;i>0;i--) {
-
 			x[i] = x[i-1];
-
 			y[i] = y[i-1];
-
 		}
-
-		
-
 		switch(direction) {
-
 		case 'U':
-
 			y[0] = y[0] - UNIT_SIZE;
-
 			break;
-
 		case 'D':
-
 			y[0] = y[0] + UNIT_SIZE;
-
 			break;
-
 		case 'L':
-
 			x[0] = x[0] - UNIT_SIZE;
-
 			break;
-
 		case 'R':
-
 			x[0] = x[0] + UNIT_SIZE;
-
 			break;
-
 		}
-
-		
-
 	}
-
 	public void checkApple() {
-
 		if((x[0] == appleX) && (y[0] == appleY)) {
-
 			bodyParts++;
-
 			applesEaten++;
-
 			newApple();
-
 		}
-
 	}
-
 	public void checkCollisions() {
-
-		//checks if head collides with body
-
-		for(int i = bodyParts;i>0;i--) {
-
+		for(int i = bodyParts; i>0; i--) {
 			if((x[0] == x[i])&& (y[0] == y[i])) {
-
 				running = false;
-
 			}
-
 		}
-
 		//check if head touches left border
-
 		if(x[0] < 0) {
-
 			running = false;
-
 		}
 
 		//check if head touches right border
