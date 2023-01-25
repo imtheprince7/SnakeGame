1327c61622c70c79cd1b9b2231221f6a27ba0a10
Prince <princejnu7@hotmail.com>
2023-01-25 09:03:33 +0530

    new update

--- src/SnakePanel.java
+++ src/SnakePanel.java
@@ -157,76 +157,39 @@ public class SnakePanel extends JPanel implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-
-		
-
 		if(running) {
-
 			move();
-
 			checkApple();
-
 			checkCollisions();
-
 		}
-
 		repaint();
-
 	}
-
-	
 
 	public class MyKeyAdapter extends KeyAdapter{
-
 		@Override
-
 		public void keyPressed(KeyEvent e) {
-
 			switch(e.getKeyCode()) {
-
 			case KeyEvent.VK_LEFT:
-
 				if(direction != 'R') {
-
 					direction = 'L';
-
 				}
-
-				break;
-
+			break;
 			case KeyEvent.VK_RIGHT:
-
 				if(direction != 'L') {
-
 					direction = 'R';
-
 				}
-
-				break;
-
+			break;
 			case KeyEvent.VK_UP:
-
 				if(direction != 'D') {
-
 					direction = 'U';
-
 				}
-
-				break;
-
+			break;
 			case KeyEvent.VK_DOWN:
-
 				if(direction != 'U') {
-
 					direction = 'D';
-
 				}
-
-				break;
+			break;
 			}
-
 		}
-
 	}
-
 }
